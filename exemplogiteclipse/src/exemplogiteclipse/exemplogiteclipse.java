b5294a31f9babe5f715239650f7da222025bd8ac
AESH9 <ar50gyn13@gmail.com>
2017-11-25 02:45:14 -0200

    outra alteração

--- exemplogiteclipse/src/exemplogiteclipse/exemplogiteclipse.java
+++ exemplogiteclipse/src/exemplogiteclipse/exemplogiteclipse.java
@@ -5,8 +5,8 @@ public class exemplogiteclipse {
 	public static void main(String[] args) {
 		System.out.print("Integrando git no eclipse");
 		//mudei do github
-		//alterado do eclipse
-		//muito pratico
+		//alterado do eclipse.
+		//muito pratico.
 	}
 
 }
