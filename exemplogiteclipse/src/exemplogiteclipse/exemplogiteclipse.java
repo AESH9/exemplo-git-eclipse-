18075072c80def969b5ab731a76f937aa78893ed
AESH9 <ar50gyn13@gmail.com>
2017-11-27 20:45:57 -0200

    atividade

--- exemplogiteclipse/src/exemplogiteclipse/exemplogiteclipse.java
+++ exemplogiteclipse/src/exemplogiteclipse/exemplogiteclipse.java
@@ -4,9 +4,9 @@ public class exemplogiteclipse {
 
 	public static void main(String[] args) {
 		System.out.print("Integrando git no eclipse");
-		//mudei do github
+		//mudei do github.
 		//alterado do eclipse.
-		//entregue a atividade
+		//entregue a atividade.
 	}
 
 }
